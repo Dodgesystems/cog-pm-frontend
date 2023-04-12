437f4a73050a80b87c15cef82145743da0997c86
victor <victoroyiga01@gmail.com>
2023-04-12 08:22:55 +0100

    12-4-2023

--- cog-frontend/src/pages/applicationmanager.js
+++ cog-frontend/src/pages/applicationmanager.js
@@ -1,56 +1,71 @@
-import React, { useState } from "react";
-// import Link from 'next/link';
-// import MultiStep from "react-multistep";
-// import AllList from "../../components/ApplicationManagers/AllList";
-// import ApplyList from "../../components/ApplicationManagers/ApplyList";
-// import PendingList from "../../components/ApplicationManagers/PendingList";
+"use client"
+
+
+import { useState, useEffect } from "react";
 import applications from '../../Data/applications'
 
 
 
 //path - http://localhost:3000/applicationmanager
 
-export default function ApplicatonManager({ applications }) {
+export default function ApplicatonManager() {
 
-  const [showData, setShowData] = useState(false);
+  const [applications, setApplications] = useState([]);
 
-  const handleClick = () => {
-    setShowData(!showData);
+  const fetchApplications = async () => {
+    const response = await fetch(`/api/applications`);
+    const data = await response.json();
+    setApplications(data);
   };
-  const handleClicks = () => {
-    setShowData(!showData);
-  };
-  const handleClickss = () => {
-    setShowData(!showData);
-  };
+  useEffect(() => {
+    fetchApplications();
+  }, []);
 
-  const applicationDate = applications?.applicationDate;
-  if (applicationDate !== undefined) {
-    console.log("we")
-  } else {
-    console.log("yes")
+
+
+
+
+  // function handleClick(event) {
+  //   const buttonClicked = event.target.name;
+
+  //   if (buttonClicked === 'All') {
+  //     const filtered = applications.filter(item => item.property === 'All');
+  //      setApplications(filtered);
+  //   } else if (buttonClicked === 'ApplyList') {
+  //     const filtered = applications.filter(item => item.property === 'ApplyList');
+  //      setApplications(filtered);
+  //   }else if(buttonClicked === 'pending'){
+  //    const filtered = applications.filter(item => item.property === 'pending');
+  //     setApplications(filtered);
+  //   }
+  // }
+
+
+    
+
+  const handleClick = (e) => {
+    let word = e.target.value;
+   
+
+    if (word === 'All') {
+      setApplications(applications)
+    } else if (word === 'ApplyList') {
+      const filtered = applications.filter(item => item.Property === 'ApplyList')
+      setApplications(filtered)
+    } else if (word === 'pending') {
+      const filtered = applications.filter(item => item.Property === 'pending')
+      setApplications(filtered)
+    }
   }
-
-
-
-
 
   return (
 
-    <main >
+    <main className='mb-5'>
       <h1 className='font-bold text-2xl px-[100px] mt-[100px] mb-[30px]'> Application Manager</h1>
       <section>
-        {/* className={{
-          style: {
-            border: "1px solid blue",
-            height: '750px',
-            width: '300px',
-            height: '300px',
-          },
-        }} */}
         <div style={{
-          border: "1px solid blue",
-          height: '770px',
+          border: "2px solid #62909F",
+          height: '700px',
           width: '900px',
           height: '700px',
           margin: "0 auto",
@@ -58,49 +73,53 @@ export default function ApplicatonManager({ applications }) {
         }}>
           <div style={{
             marginTop: "60px",
-            // marginLeft: '40px',
             gap: "20px",
-            borderBottom: "1px solid blue",
-
+            borderBottom: "3px solid #62909F",
           }}>
-            <div style={{ marginBottom: "20px" }}>
-              <button onClick={handleClick} style={{ paddingRight: "70px",paddingLeft: "70px", fontWeight: "bold"}}>AllList</button>
-              <button onClick={handleClicks} style={{ paddingRight: "70px", fontWeight: "bold"}}>ApplyList</button>
-              <button onClick={handleClickss} style={{ paddingRight: "30px", fontWeight: "bold"}}>Pending</button>
-            </div>
-          </div>
-          <div className="flex flex-row mb-4 gap-4 ml-4 mt-4 ">
-          <div>
-            <div className="flex w-[120px] h-[40px] border border-indigo-600 justify-center items-center">
-                <select name="applicationType">
-                <option> data  </option>
-                <option value="Individual"> month </option>
-                <option value="Company"> Company </option>
-              </select>
-            </div>
-          </div>
-          <div className="flex w-[120px] h-[40px]  border border-indigo-600 justify-center items-center">
+         <div style={{ marginBottom: "20px" }}>
+              <button   value='All' onClick={handleClick} style={{ paddingRight: "70px", paddingLeft: "70px", fontWeight: "bold" }}>All</button>
+              <button value='ApplyList' onClick={handleClick} style={{ paddingRight: "70px", fontWeight: "bold" }}>ApplyList</button>
+              <button  value='pending' onClick={handleClick} style={{ paddingRight: "30px", fontWeight: "bold" }}>Pending</button>
+            </div> 
+        </div>
+         <div className="flex flex-row mb-4 gap-4 ml-4 mt-4 ">
             <div>
+              <div className="flex w-[120px] h-[40px] border border-[#62909F] justify-center items-center">
+                <select name="applicationType ">
+                  <option> date  </option>
+                  <option value="Individual"> month </option>
+                  <option value="Company"> date </option>
+                </select>
+              </div>
+            </div>
+            <div className="flex w-[120px] h-[40px]  border border-[#62909F] justify-center items-center">
+              <div>
                 <select name="applicationType">
-                <option> Year  </option>
-                <option value="Individual"> month </option>
-                <option value="Company"> day </option>
-              </select>
+                  <option> Year  </option>
+                  <option value="Individual"> month </option>
+                  <option value="Company"> day </option>
+                </select>
+              </div>
             </div>
           </div>
-          </div>
-          {showData ? (
-            <p>Here is the data that should be displayed when the button is clicked.
-              rrrrwrwrwrwwtwtwywyw
-              gdggdggdgdgdggdgdgggdgdgd hhhfhfjdjud
-              dfdfdgd hhddhdhdhdn  fsgshsjnf
-              cvbnjyf gdgdgdhhdyd qeewwwiow
-              fsfrwtwyueioee bgfhfuewopwks njd
-            </p>
-          ) : (
-            <p>No data to display yet.</p>
-          )}
 
+         <div>
+         {applications.map((application) => {
+            return (
+             
+               <div key={application.id} style={{ marginTop: "10px", overFlowY: 'hidden',display: 'flex', paddingRight: '30px' ,justifyContent: 'center', alignItem: 'center'}}>
+                  <div style={{  paddingRight: '30px'}}>
+                    <input type="checkbox" />
+                  </div>
+                  <p style={{  paddingRight: '30px',   paddingBottom: '25px'}}>{application.applicationDate}</p>
+                  <p style={{  paddingRight: '30px' ,paddingBottom: '25px'}}>{application.listName}</p>
+                  <p style={{  paddingRight: '30px', paddingBottom: '25px' }}>{application.propertyName}</p>
+                  <p style={{  paddingRight: '30px', paddingBottom: '25px' }}>{application.rentalpropertyamount}</p>
+            
+              </div> 
+            );
+          })}
+         </div>
         </div>
       </section>
     </main>
